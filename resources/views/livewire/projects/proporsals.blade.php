a92da9845f876b5bf65a26ffed330f6d4db4a5ea
diegofernandesss <fernandes.diego@academico.ifpb.edu.br>
2024-11-07 14:32:17 -0300

    criando livewire components

---
+++ resources/views/livewire/projects/proporsals.blade.php
@@ -0,0 +1,3 @@
+<div>
+    componente livewire project proporsals
+</div>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app>
+
+    <livewire:projects.show :$project/>
+
+    <livewire:projects.proporsals :$project />
+
+</x-layouts.app>
