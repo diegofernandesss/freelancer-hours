c278608d8d66d54cf1d3e616fa8f0594c1d13d9f
diegofernandesss <fernandes.diego@academico.ifpb.edu.br>
2024-11-11 21:49:03 -0300

    Salvando proposta

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -19,16 +19,28 @@ class Create extends Component
     #[Rule(['required', 'numeric', 'gt:0'])]
     public int $hours = 0;
 
+    public bool $agree = false;
+
     public function save()
     
     {
+        
         $this->validate();
+        
+        if(!$this->agree) {
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+
+            return;
+        }
 
         $this->project->proposals()
             ->updateOrCreate(
             ['email' => $this->email],
             ['hours' => $this->hours]
         );
+
+
+        $this->modal = false;
     }
 
 
--- resources/views/livewire/proposals/create.blade.php
+++ resources/views/livewire/proposals/create.blade.php
@@ -24,28 +24,27 @@
                             placeholder="Insira o seu e-mail" />
 
                         @error('email')
-                            <div class="text-red-600 mt-1 text-sm">{{ $message }}</div>
+                            <div class="mt-1 text-sm text-red-600">{{ $message }}</div>
                         @enderror
 
                     </div>
                     <div class="flex w-1/3 flex-col gap-2">
-                        <label class="text-[14px] text-[#C3C3D1]">Horas</label>
+                        <label class="text-[14px] text-[#C3C3D1]">
+                            Horas
+                        </label>
                         <div class="flex" x-data="{ hours: @entangle('hours') }">
-                            <button 
-                                type="button"
+                            <button type="button"
                                 class="bg-[#1E1E2C] px-3 py-2 text-3xl text-[#C3C3D1] transition duration-300 ease-in-out hover:bg-[#313145]"
                                 @click="hours--">-</button>
                             <input wire:model="hours" type="number"
-                                class="w-[40px] border border-[#1E1E2C] bg-[#1E1E2C] py-2 pl-3 font-bold text-white focus:outline-none focus:ring-0 focus:ring-blue-500"
-                                />
-                            <button
-                                type="button"
+                                class="w-[40px] border border-[#1E1E2C] bg-[#1E1E2C] py-2 pl-3 font-bold text-white focus:outline-none focus:ring-0 focus:ring-blue-500" />
+                            <button type="button"
                                 class="bg-[#1E1E2C] px-3 py-2 text-3xl text-[#C3C3D1] transition duration-300 ease-in-out hover:bg-[#313145]"
                                 @click="hours++">+</button>
                         </div>
 
                         @error('hours')
-                            <div class="text-red-600 mt-1 text-sm">{{ $message }}</div>
+                            <div class="mt-1 text-sm text-red-600">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
@@ -53,7 +52,7 @@
                     <label for="agree"
                         class="flex cursor-pointer items-center gap-2 text-sm font-medium text-neutral-600 dark:text-neutral-300 [&:has(input:checked)]:text-neutral-900 dark:[&:has(input:checked)]:text-white [&:has(input:disabled)]:cursor-not-allowed [&:has(input:disabled)]:opacity-75">
                         <div class="relative flex items-center">
-                            <input id="agree" type="checkbox"
+                            <input wire:model="agree" id="agree" type="checkbox"
                                 class="before:content[''] size-4 peer relative cursor-pointer appearance-none overflow-hidden rounded border border-neutral-300 bg-neutral-50 before:absolute before:inset-0 checked:border-[#5354FD] checked:before:bg-[#5354FD] focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-neutral-800 checked:focus:outline-[#5354FD] active:outline-offset-0 disabled:cursor-not-allowed dark:border-neutral-700 dark:bg-neutral-900 dark:checked:border-[#5354FD] dark:checked:before:bg-[#5354FD] dark:focus:outline-neutral-300 dark:checked:focus:outline-[#5354FD]" />
                             <x-ui.icons.check
                                 class="size-3 pointer-events-none invisible absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white peer-checked:visible dark:text-white" />
@@ -61,6 +60,10 @@
                         <span class="text-[14px] text-[#8C8C9A]">Concordo com os Termos e Políticas de
                             privacidade.</span>
                     </label>
+
+                    @error('agree')
+                            <div class="mt-1 text-sm text-red-600">{{ $message }}</div>
+                    @enderror
                 </div>
                 <button
                     class="w-full rounded-[4px] bg-[#5354FD] px-8 py-3 font-bold uppercase tracking-wide text-white transition duration-300 ease-in-out hover:bg-[#1f20a6]">
